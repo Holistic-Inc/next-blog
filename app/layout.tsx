600941a8ac8c139a13b1a72881c569e4a2282a55
Jakub Matyka <jaqbec@gmail.com>
2023-09-23 22:57:19 +0200

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <div className="relative flex min-h-screen flex-col">{children}</div>
         <div>Is it home page: {home ? "it is" : "it's not"}</div>
+        <Analytics />
       </body>
     </html>
   );
